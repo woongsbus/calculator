01d468e7f998119a9ce4c1dc3012b178fe82caef
woongsbus <choigun0423@gmail.com>
2026-08-03 19:35:03 +0900

    commit study

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox)
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox,QPlainTextEdit)
 from PyQt5.QtGui import QIcon
 
 class Calculator(QWidget):
@@ -9,11 +9,13 @@ class Calculator(QWidget):
         self.initUI()
 
     def initUI(self):
+        self.te1 = QPlainTextEdit()
+        self.te1.setReadOnly(True)
         self.btn1=QPushButton('Message',self)
         self.btn1.clicked.connect(self.activateMessage)
 
         vbox=QVBoxLayout()
-        vbox.addStretch(1)
+        vbox.addWidget(self.te1)
         vbox.addWidget(self.btn1)
         vbox.addStretch(1)
 
@@ -25,7 +27,8 @@ class Calculator(QWidget):
         self.show()
 
     def activateMessage(self):
-        QMessageBox.information(self,"information","Button clicked!")
+        #QMessageBox.information(self,"information","Button clicked!")
+        self.te1.appendPlainText("Button clicked!")
 
 
 if __name__=='__main__':
